d3d876c8170bf630bd5c5833ed3db80c1d490e90
ApocalypseCalculator <apocalypsecalculator@gmail.com>
2021-06-23 18:10:25 -0400

    config port

--- index.js
+++ index.js
@@ -100,7 +100,7 @@ io.on("connection", (socket) => {
     });
 })
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || config.port;
 server.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 });
