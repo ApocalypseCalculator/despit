8745fb1b121873fa2272f294239f3b851847d714
ApocalypseCalculator <apocalypsecalculator@gmail.com>
2021-06-23 19:01:22 -0400

    add notifs

--- cdn/script.js
+++ cdn/script.js
@@ -15,6 +15,9 @@ setname.addEventListener('submit', function (e) {
 });
 
 var jwt = "";
+var totalmsg = 0;
+var focused = true;
+var basetitle = document.title;
 
 form.addEventListener('submit', function (e) {
   e.preventDefault();
@@ -31,6 +34,7 @@ socket.on('message', function (msg) {
   item.innerHTML = `<b>${msg.user}:</b> ${msg.msg}`;
   messages.appendChild(item);
   window.scrollTo(0, document.body.scrollHeight);
+  newmsg();
 });
 socket.on('join', function (msg) {
   msg = escapeHtml(msg);
@@ -38,6 +42,7 @@ socket.on('join', function (msg) {
   item.innerHTML = `<b>[system]:</b> A user <b>${msg}</b> joined the chat room`;
   messages.appendChild(item);
   window.scrollTo(0, document.body.scrollHeight);
+  newmsg();
 });
 socket.on('leave', function (msg) {
   msg = escapeHtml(msg);
@@ -45,12 +50,14 @@ socket.on('leave', function (msg) {
   item.innerHTML = `<b>[system]</b>: A user <b>${msg}</b> has left the chat`;
   messages.appendChild(item);
   window.scrollTo(0, document.body.scrollHeight);
+  newmsg();
 });
 socket.on('error', function (msg) {
   var item = document.createElement('li');
   item.innerHTML = `<b>[system]</b>: Error! <b>${msg}</b>`;
   messages.appendChild(item);
   window.scrollTo(0, document.body.scrollHeight);
+  newmsg();
 });
 socket.on('jwt', function (msg) {
   jwt = msg;
@@ -60,6 +67,23 @@ socket.on('jwt', function (msg) {
   setname.remove();
 });
 
+function newmsg() {
+  if (!focused) {
+    totalmsg++;
+    document.title = `(${(totalmsg > 99) ? `99+` : totalmsg}) ${basetitle}`;
+  }
+}
+
 function escapeHtml(unsafe) {
   return unsafe.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#039;");
+}
+
+window.onfocus = function () {
+  document.title = basetitle;
+  totalmsg = 0;
+  focused = true;
+}
+
+window.onblur = function () {
+  focused = false;
 }
